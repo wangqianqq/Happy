34474d1b19b4f581ca9d1187343804f06af7d8d8
wangqianqq <zhuanjiaoqianqian@gmail.com>
2014-12-07 20:04:03 +0800

    静态工具类

---
+++ JavaSE/Day06/Test.java
@@ -0,0 +1,8 @@
+public class Test{
+	public static void main(String[] args){
+		int[] arr={4344,555,44,3,3,32334,4};
+		ArrayOperate A=new ArrayOperate();
+		System.out.println(A.max(arr));
+	}
+	
+}
