48c66353b026b990add126b1ea525724eea71d85
wangqianqq <zhuanjiaoqianqian@gmail.com>
2014-12-12 00:05:34 +0800

    匿名内部类

---
+++ JavaSE/niming/A.java
@@ -0,0 +1,27 @@
+abstract class Student{
+	 abstract void speak();
+
+}
+
+class Teacher{
+	void look(Student stu){
+		stu.speak();//执行匿名类体中重写的speak()方法
+		
+	}
+
+}
+
+public class A{
+	public static void main(String[] args){
+		Teacher zhang=new Teacher();
+		zhang.look(new Student(){   //匿名类的类体，即Student的子类的类体
+			void speak(){
+				System.out.println("这是匿名类体重的方法");
+			}
+			
+	    }); //匿名类类体结束
+                   
+    }
+
+}
+
